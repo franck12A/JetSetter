195c1120d77ad505e789d7a95893d42911decf42
franck12A <enricifranco6@gmail.com>
2026-03-11 17:44:34 -0300

    Make footer stick to bottom

--- reservas-frontend/src/main.jsx
+++ reservas-frontend/src/main.jsx
@@ -14,10 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <Router>
-        <div
-          className="app-container"
-          style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
-        >
+        <div className="app-container">
           <App />
           <Footer />
         </div>
